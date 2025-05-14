35cc776b489208a5c31f3a5af62f6e17fe1c0181
MaxVorosh <ma_voroshilov@mail.ru>
2025-05-14 13:33:20 -0400

    Add time measurement

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,6 +2,7 @@ import java.util.Vector;
 
 public class Runner {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         FileWorker worker = new FileWorker();
         worker.writeTokensDir(args[0], "");
         Processor processor = new Processor(7, 0.1f, 0.5f, 0.4f, 0.65f, "./index"); // Didn't find k value in paper
@@ -9,5 +10,7 @@ public class Runner {
         Vector<CodeBlock> blocks = worker.parseDir("./tokens");
         Vector<ClonePair> clones = processor.getClonePairs(blocks);
         worker.writeReport(clones, "./report");
+        long endTime = System.currentTimeMillis();
+        System.out.println(String.format("%dms", endTime - startTime));
     }
 }
