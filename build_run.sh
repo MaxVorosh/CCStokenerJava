ea3bf6dd5ef32a3645031839cdc03a3f96d46600
MaxVorosh <ma_voroshilov@mail.ru>
2025-05-28 14:06:59 -0400

    Change running script

--- build_run.sh
+++ build_run.sh
@@ -1,5 +1,10 @@
-mkdir index
-mkdir tokens
-sh gradlew run --args=\'$1\'
 rm -rf index
+rm -rf indexForSmall
+rm -rf tokens
+mkdir index
+mkdir indexForSmall
+mkdir tokens
+sh gradlew run --args=\'$1\' --no-daemon -Dorg.gradle.jvmargs="-Xms1024m -Xmx2048m"
+rm -rf index
+rm -rf indexForSmall
 rm -rf tokens
